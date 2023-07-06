bd3f9f38e0c52e3c6f3b9b2d5a1dfcb0f812739c
wooobo <irooori89@gmail.com>
2023-07-06 13:00:50 +0900

    feat: `meta` 정보 활용으로 변경
    - EntityLoader

--- src/main/java/persistence/sql/ddl/EntityLoader.java
+++ src/main/java/persistence/sql/ddl/EntityLoader.java
@@ -6,39 +6,44 @@ import jdbc.RowMapper;
 
 import java.lang.reflect.Field;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-public class ReflectiveRowMapper<T> implements RowMapper<T> {
+public class EntityLoader<T> implements RowMapper<T> {
     private final Class<T> targetType;
+    private final Map<String, String> columnMap;
 
-    public ReflectiveRowMapper(Class<T> targetType) {
+    public EntityLoader(Class<T> targetType) {
         this.targetType = targetType;
+        this.columnMap = Arrays.stream(targetType.getDeclaredFields())
+                .collect(Collectors.toMap(this::columnName, Field::getName));
+
     }
 
     @Override
     public T mapRow(ResultSet resultSet) throws SQLException {
+        ResultSetMetaData meta = resultSet.getMetaData();
+        int columnCount = meta.getColumnCount();
         T targetObject = null;
         try {
             targetObject = targetType.getDeclaredConstructor().newInstance();
-
-            Field[] fields = fields(targetType.getDeclaredFields());
-            for (Field field : fields) {
-                String columnName = columnName(field);
-                Object value = resultSet.getObject(columnName);
+            
+            for (int i = 0; i < columnCount; i++) {
+                String alias = meta.getColumnLabel(i + 1);
+                String name = meta.getColumnName(i + 1);
+                Field field = targetType.getDeclaredField(columnMap.get(alias.toLowerCase()));
                 field.setAccessible(true);
+
+                Object value = resultSet.getObject(name);
                 field.set(targetObject, value);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
         return targetObject;
-    }
-
-    private Field[] fields(Field[] fields) {
-        return Arrays.stream(fields)
-                .filter(field -> field.getAnnotation(Transient.class) == null)
-                .toArray(Field[]::new);
     }
 
     private String columnName(Field field) {
