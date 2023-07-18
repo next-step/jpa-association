5b1119fc646c5762452e95a3f4d8a9b075f31c01
leeyohan93 <good_1411@naver.com>
2023-07-19 01:04:31 +0900

    feat: OneToMany 미션 도메인 모델 추가

---
+++ src/main/java/model/OrderItem.java
@@ -0,0 +1,16 @@
+package model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String product;
+
+    private Integer quantity;
+}
