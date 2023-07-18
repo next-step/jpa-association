bacc99ae8dfc212a7c7ef1ec4547ba2d0c16f697
Pyro(JeongWan Gho) <rhwjddhks@gmail.com>
2023-07-18 14:09:47 +0900

    feat: OrderItem 과 Order 엔티티를 domain 에 추가

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,27 @@
+package domain;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> orderItems;
+}
