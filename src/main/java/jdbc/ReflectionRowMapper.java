6c31032a8f5374a6a388b73b490ae762c98ef778
jinyoungchoi95 <jinyoungchoi95@woowahan.com>
2023-11-03 17:32:25 +0900

    feat: RowMapper join column mapping 추가 및 테스트

--- src/main/java/jdbc/ReflectionRowMapper.java
+++ src/main/java/jdbc/ReflectionRowMapper.java
@@ -2,9 +2,11 @@ package jdbc;
 
 import hibernate.entity.meta.EntityClass;
 import hibernate.entity.meta.column.EntityColumn;
+import hibernate.entity.meta.column.EntityJoinColumn;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -28,8 +30,27 @@ public class ReflectionRowMapper<T> implements RowMapper<T> {
         T instance = entityClass.newInstance();
         List<EntityColumn> entityColumns = entityClass.getEntityColumns();
         for (EntityColumn entityColumn : entityColumns) {
-            entityColumn.assignFieldValue(instance, resultSet.getObject(entityColumn.getFieldName()));
+            entityColumn.assignFieldValue(instance, resultSet.getObject(entityClass.tableName() + "." + entityColumn.getFieldName()));
         }
+
+        List<EntityJoinColumn> eagerJoinColumns = entityClass.getEagerJoinColumn();
+        if (eagerJoinColumns.isEmpty()) {
+            return instance;
+        }
+        do {
+            for (EntityJoinColumn eagerJoinColumn : eagerJoinColumns) {
+                EntityClass<?> eagerJoinClass = eagerJoinColumn.getEntityClass();
+
+                List<Object> subInstances = new ArrayList<>();
+                Object subInstance = eagerJoinClass.newInstance();
+                List<EntityColumn> subEntityColumns = eagerJoinClass.getEntityColumns();
+                for (EntityColumn subEntityColumn : subEntityColumns) {
+                    subEntityColumn.assignFieldValue(subInstance, resultSet.getObject(eagerJoinClass.tableName() + "." + subEntityColumn.getFieldName()));
+                }
+                subInstances.add(subInstance);
+                eagerJoinColumn.assignFieldValue(instance, subInstances);
+            }
+        } while (resultSet.next());
         return instance;
     }
 }
