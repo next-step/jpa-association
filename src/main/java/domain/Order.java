5a1cce79c2c4e3b2e2a260c69efc1b4cf812e9af
beng9re <kbh0581@gmail.com>
2023-11-04 21:03:23 +0900

    chore : 미션을 위한 기본 객체 생성

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,30 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> orderItems;
+
+//    @OneToMany
+//    @JoinColumn(name = "order_item_id2")
+//    private List<OrderItem2> orderItem2;
+}
---
+++ src/main/java/domain/OrderItem.java
@@ -0,0 +1,25 @@
+package domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import java.util.List;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String product;
+
+    private Integer quantity;
+
+}
