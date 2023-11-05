e02cb5c213b55f3a0a94ce3b2a6fe1f0400e0f1b
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-05 11:45:42 +0900

    feat) setJoinTargets 메서드 설계

--- src/main/java/persistence/entity/EntityLoader.java
+++ src/main/java/persistence/entity/EntityLoader.java
@@ -5,6 +5,9 @@ import jdbc.JdbcTemplate;
 import persistence.sql.dialect.Dialect;
 import persistence.sql.dialect.DialectFactory;
 import persistence.sql.dml.DmlQueryGenerator;
+import persistence.sql.meta.ColumnMetas;
+import persistence.sql.meta.EntityMeta;
+import persistence.sql.meta.MetaFactory;
 
 public class EntityLoader {
 
@@ -25,6 +28,24 @@ public class EntityLoader {
 
     public <T> T selectOne(Class<T> clazz, Long id) {
         String selectByPkQuery = dmlQueryGenerator.generateSelectByPkQuery(clazz, id);
-        return jdbcTemplate.queryForObject(selectByPkQuery, new EntityRowMapper<>(clazz));
+        T entity = jdbcTemplate.queryForObject(selectByPkQuery, new EntityRowMapper<>(clazz));
+        EntityMeta entityMeta = MetaFactory.get(clazz);
+        ColumnMetas columnMetas = entityMeta.getColumnMetas();
+        if (columnMetas.hasJoinEntity()) {
+            setJoinTargets(entity, id);
+        }
+        return entity;
+    }
+
+    private <T> void setJoinTargets(T entity, Long id) {
+        String selectQuery = dmlQueryGenerator.generateSelectWithJoinByPkQuery(entity.getClass(), id);
+        EntityMeta entityMeta = MetaFactory.get(entity.getClass());
+        ColumnMetas columnMetas = entityMeta.getColumnMetas();
+        columnMetas.forEach(columnMeta -> {
+            if (columnMeta.isJoinColumn()) {
+                // Join table 이름으로 자식 엔티티 목록을 매핑조회한다
+                // 조회한 인스턴스를 부모 인스턴스에 세팅한다
+            }
+        });
     }
 }
