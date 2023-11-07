b9062a52f32cdced8b7974aa4b95a4e92c89233c
limdingdong <limdingdong@imdongseob-ui-MacBookAir.local>
2023-11-07 12:58:01 +0900

    refactor) exceptTransient, exceptJoin 메서드체이닝 적용

--- src/main/java/persistence/sql/ddl/builder/ColumnBuilder.java
+++ src/main/java/persistence/sql/ddl/builder/ColumnBuilder.java
@@ -29,11 +29,11 @@ public class ColumnBuilder {
 
     private List<String> toSql() {
         List<String> sqlElements = new ArrayList<>();
-        ColumnMetas exceptTransientColumns = columnMetas.exceptTransient();
-        ColumnMetas exceptJoin = exceptTransientColumns.exceptJoin();
-        exceptJoin.forEach(
-                columnMeta -> sqlElements.add(toSql(columnMeta))
-        );
+        columnMetas.exceptTransient()
+                .exceptJoin()
+                .forEach(
+                        columnMeta -> sqlElements.add(toSql(columnMeta))
+                );
         return sqlElements;
     }
 
