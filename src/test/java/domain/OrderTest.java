703c97e4c88e99dd5ecbe037256d28612fcf7dd2
zinzoddari <zinzoddari@gmail.com>
2023-11-09 16:38:30 +0900

    test: order 테이블 성공적으로 조회하여 결과 검증

--- src/test/java/domain/OrderTest.java
+++ src/test/java/domain/OrderTest.java
@@ -5,6 +5,7 @@ import database.H2;
 import domain.helper.CreateSqlHelper;
 import jdbc.JdbcTemplate;
 import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +16,7 @@ import java.sql.SQLException;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.SoftAssertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class OrderTest {
@@ -22,15 +24,15 @@ class OrderTest {
     private JdbcTemplate jdbcTemplate;
     private EntityManager entityManager;
 
+    private Order order = new Order(1L, "00001", null);
+
     @BeforeEach
     void init() throws SQLException {
         테이블_생성();
 
-        Order order = new Order(1L, "00001", null);
         OrderItem item1 = new OrderItem(30L, "빼빼로", 3);
         OrderItem item2 = new OrderItem(55L, "새우깡", 1);
         OrderItem item3 = new OrderItem(77L, "사또밥", 2);
-
 
         jdbcTemplate.execute(CreateSqlHelper.주문을_생성하는_쿼리_생성(order));
         jdbcTemplate.execute(CreateSqlHelper.주문아이템을_생성하는_쿼리_생성(item1, order));
@@ -41,10 +43,20 @@ class OrderTest {
     }
 
     @Test
-    @DisplayName("오류 없이 join select가 되는지 확인")
+    @DisplayName("order를 조회하여 정상적으로 생성함")
     void ii() {
-        //when & then
-        assertDoesNotThrow(() -> entityManager.find(Order.class, 1L));
+        //given
+        final Long orderId = 1L;
+        final String orderNumber = "00001";
+
+        //when
+        Order result = entityManager.find(Order.class, 1L);
+
+        //then
+        assertSoftly(softAssertions -> {
+            softAssertions.assertThat(result.getId()).isEqualTo(orderId);
+            softAssertions.assertThat(result.getOrderNumber()).isEqualTo(orderNumber);
+        });
     }
 
     private void 테이블_생성() throws SQLException {
