f5347be82cbf17003f89b200c044245162f677e4
hhhhhwi <aab555586@gmail.com>
2023-11-11 02:14:59 +0900

    feat: Order, OrderItem 객체 생성

---
+++ src/main/java/domain/Order.java
@@ -0,0 +1,21 @@
+package domain;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String orderNumber;
+
+	@OneToMany(fetch = FetchType.EAGER)
+	@JoinColumn(name = "order_id")
+	private List<OrderItem> orderItems;
+}
+
