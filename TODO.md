33362efb2c96f936e2b4b4095d8e815cab9fd8bc
Dongyoon Lee <nine.cow@gmail.com>
2024-03-07 01:33:51 +0900

    test: Application 에서 테스트

--- TODO.md
+++ TODO.md
@@ -1,7 +1,9 @@
 # 개선할 만한 부분
 
 - 생성자에 들어있는 로직들을 별도 메서드로 분리해내기(생성자는 생성만 하고 끝마치도록)
-
+- EntityManager 의 역할이 너무 많다.
+- 로직을 너무 객체 만들기 직전에 하려는 경향이 있는 것 같다
+- 
 # 구현 피드백
 
 - PersistenceContext 의 상태값 중에 entitiesByKey, entitySnapshotsByKey 가 별도로 있는 이유 <- 필요할 때 도입
@@ -17,5 +19,13 @@
 * 일관되게 작성했나? (비슷한 일을 하는 클래스가 여러 스타일로 작성됐는가?)
 * 불필요하게 쪼개져있진 않은가?
 * public/private modifier 기준 소트
-* 객체가 할만한 일은 객체에게 맡기기
+* 객체가 할만한 일은 객체에게 맡기기(getter/setter 호출하는 대신 하위 객체에게 맡길 일이 있는지)
+* 디미터 법칙을 잘 지켜보기
+
+# 읽어볼 아티클
+
+* https://jessyt.tistory.com/134
+* https://reflectoring.io/do-not-use-checked-exceptions/
+* https://www.baeldung.com/parameterized-tests-junit-5
+* https://developerfarm.wordpress.com/2012/02/03/object_calisthenics_summary/
 * 
--- src/main/java/persistence/Application.java
+++ src/main/java/persistence/Application.java
@@ -2,7 +2,10 @@ package persistence;
 
 import database.DatabaseServer;
 import database.H2;
-import jdbc.JdbcTemplate;
+import database.sql.dml.CustomSelect;
+import entity.Order;
+import entity.OrderItem;
+import entity.Person;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +18,27 @@ public class Application {
             final DatabaseServer server = new H2();
             server.start();
 
-            final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
+//            final JdbcTemplate jdbcTemplate = new JdbcTemplate(server.getConnection());
+            System.out.println(new CustomSelect(Order.class).buildQuery());
+            System.out.println(new CustomSelect(Person.class).buildQuery());
+            System.out.println(new CustomSelect(OrderItem.class).buildQuery());
+
+//            MySQLDialect dialect = MySQLDialect.getInstance();
+//            jdbcTemplate.execute(QueryBuilder.getInstance().buildCreateQuery(Order.class, dialect));
+//            System.out.println(QueryBuilder.getInstance().buildCreateQuery(Order.class, dialect));
+//            jdbcTemplate.execute(QueryBuilder.getInstance().buildCreateQuery(OrderItem.class, dialect));
+//            System.out.println(QueryBuilder.getInstance().buildCreateQuery(OrderItem.class, dialect));
+//
+//            EntityManager entityManager = EntityManagerImpl.from(jdbcTemplate);
+//            Order order = entityManager.persist(new Order("1234"));
+//            OrderItem orderItem1 = entityManager.persist(new OrderItem("product1", 5));
+//            OrderItem orderItem2 = entityManager.persist(new OrderItem("product2", 5));
+//
+//            System.out.println(order);
+//            System.out.println(orderItem1);
+//            System.out.println(orderItem2);
+//
+//            jdbcTemplate.execute(database.sql.dml.QueryBuilder.getInstance().buildCustomSelectQuery(Order.class));
 
             server.stop();
         } catch (Exception e) {
@@ -24,4 +47,5 @@ public class Application {
             logger.info("Application finished");
         }
     }
+
 }
