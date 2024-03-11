3f06a85149e29cfcfd89e671864e0119dfb44b6a
ckyeon <qsky8520@gmail.com>
2024-03-11 11:50:18 +0900

    feat(OrderItem): OrderItem 엔티티 추가

---
+++ src/test/java/persistence/study/sql/ddl/OrderItem.java
@@ -0,0 +1,16 @@
+package persistence.study.sql.ddl;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String product;
+
+    private Integer quantity;
+}
