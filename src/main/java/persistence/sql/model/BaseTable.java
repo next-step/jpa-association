afe6c1a443748c89dce48fceb4ce689b6363e534
ckyeon <qsky8520@gmail.com>
2024-03-12 22:07:35 +0900

    feat(BaseTable): 메서드 추가

--- src/main/java/persistence/sql/model/BaseTable.java
+++ src/main/java/persistence/sql/model/BaseTable.java
@@ -3,14 +3,14 @@ package persistence.sql.model;
 import java.util.List;
 
 public interface BaseTable {
+
+    Class<?> getEntity();
     
     String getName();
 
     PKColumn getPKColumn();
 
     Columns getColumns();
-
-    List<JoinTable> getJoinTables();
 
     String getPKColumnName();
 
