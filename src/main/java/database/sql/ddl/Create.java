937e91836115334a44e25c7a0fd5747274d8ba54
Dongyoon Lee <nine.cow@gmail.com>
2024-03-13 23:29:51 +0900

    refactor: 코드 정리

--- src/main/java/database/sql/ddl/Create.java
+++ src/main/java/database/sql/ddl/Create.java
@@ -12,13 +12,6 @@ public class Create {
     private final List<String> columnDefinitions;
     private final List<String> joinColumnDefinitions;
 
-    public Create(Class<?> clazz, Dialect dialect) {
-        EntityMetadata entityMetadata = EntityMetadataFactory.get(clazz);
-        this.tableName = entityMetadata.getTableName();
-        this.columnDefinitions = entityMetadata.getColumnDefinitions(dialect);
-        this.joinColumnDefinitions = List.of();
-    }
-
     public Create(Class<?> clazz, List<Class<?>> entities, Dialect dialect) {
         EntityMetadata entityMetadata = EntityMetadataFactory.get(clazz);
         this.tableName = entityMetadata.getTableName();
@@ -26,12 +19,15 @@ public class Create {
         this.joinColumnDefinitions = entityMetadata.getJoinColumnDefinitions(dialect, entities);
     }
 
+    public Create(Class<?> clazz, Dialect dialect) {
+        this(clazz, List.of(), dialect);
+    }
+
     public String buildQuery() {
         List<String> newList = new ArrayList<>();
         newList.addAll(columnDefinitions);
         newList.addAll(joinColumnDefinitions);
-        String columnsWithDefinition1 = String.join(", ", newList);
 
-        return String.format("CREATE TABLE %s (%s)", tableName, columnsWithDefinition1);
+        return String.format("CREATE TABLE %s (%s)", tableName, String.join(", ", newList));
     }
 }
