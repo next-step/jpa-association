cf0c2d2151d4a31bfe4edd40ef6b0a78862ee4dd
Dongyoon Lee <nine.cow@gmail.com>
2024-03-16 01:47:51 +0900

    test: NoOp, LazyLoader, Dispatcher, FixedValue 테스트

--- src/test/java/learning/proxy/HelloTarget.java
+++ src/test/java/learning/proxy/HelloTarget.java
@@ -1,6 +1,15 @@
 package learning.proxy;
 
+import java.util.List;
+
 public class HelloTarget {
+    public HelloTarget() {
+    }
+
+    public HelloTarget(List<String> objectNames, String name) {
+        objectNames.add(name);
+    }
+
     public String sayHello(String name) {
         return "Hello " + name;
     }
