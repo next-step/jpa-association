97c874f22b995ad719ae987728a607c04d7cb2f3
Shin seongsu <tlstjdtn321@naver.com>
2024-03-17 04:21:39 +0900

    feat: 연관관계있는 도메인 추가

---
+++ src/main/java/domain/OrderItem.java
@@ -0,0 +1,40 @@
+package domain;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String product;
+
+    private Integer quantity;
+
+    public OrderItem() {
+    }
+
+    public OrderItem(Long id, String product, Integer quantity) {
+        this.id = id;
+        this.product = product;
+        this.quantity = quantity;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        OrderItem orderItem = (OrderItem) o;
+        return Objects.equals(id, orderItem.id) && Objects.equals(product, orderItem.product) && Objects.equals(quantity, orderItem.quantity);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, product, quantity);
+    }
+}
