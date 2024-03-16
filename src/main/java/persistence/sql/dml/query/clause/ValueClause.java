639299c3ab51e8bcd32aef5e62d43c5b81bf2369
Shin seongsu <tlstjdtn321@naver.com>
2024-03-17 04:22:36 +0900

    feat: Join Entity를 만들수 있도록 추가

--- src/main/java/persistence/sql/dml/query/clause/ValueClause.java
+++ src/main/java/persistence/sql/dml/query/clause/ValueClause.java
@@ -1,17 +1,24 @@
 package persistence.sql.dml.query.clause;
 
+import jakarta.persistence.Id;
 import persistence.sql.dml.exception.IllegalFieldValueException;
+import persistence.sql.dml.exception.InvalidFieldValueException;
 import persistence.sql.dml.exception.NotFoundFieldNameException;
+import persistence.sql.entity.model.DomainType;
 import persistence.sql.entity.model.DomainTypes;
 
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static persistence.sql.constant.SqlConstant.COMMA;
+import static persistence.sql.constant.SqlConstant.LINE_COMMA;
 import static persistence.sql.constant.SqlFormat.STRING_FORMAT;
 
 public class ValueClause {
+
+    private static final int ZERO = 0;
+
     private final List<String> values;
 
     private ValueClause(final List<String> values) {
@@ -22,7 +29,12 @@ public class ValueClause {
                                    final DomainTypes domainTypes) {
         return new ValueClause(domainTypes.getDomainTypes()
                 .stream()
-                .map(domainType -> getValue(instance, getField(instance.getClass(), domainType.getName())))
+                .map(domainType -> {
+                    if (domainType.isJoinColumn()) {
+                        return getJoinEntity(instance, domainType);
+                    }
+                    return getValue(instance, getField(instance.getClass(), domainType.getName()));
+                })
                 .collect(Collectors.toList()));
     }
 
@@ -32,6 +44,28 @@ public class ValueClause {
             return clazz.getDeclaredField(fieldName);
         } catch (Exception e) {
             throw new NotFoundFieldNameException();
+        }
+    }
+
+    private static String getJoinEntity(final Object instance, final DomainType domainType) {
+        List value = getObjectValue(instance, getField(instance.getClass(), domainType.getName()));
+        Object subInstance = value.get(ZERO);
+
+        return Arrays.stream(value.get(ZERO).getClass()
+                        .getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Id.class))
+                .map(field -> getValue(subInstance, field))
+                .findFirst()
+                .orElseThrow(InvalidFieldValueException::new);
+    }
+
+    private static List getObjectValue(final Object instance,
+                                       final Field field) {
+        try {
+            field.setAccessible(true);
+            return (List) field.get(instance);
+        } catch (Exception e) {
+            throw new IllegalFieldValueException();
         }
     }
 
@@ -50,7 +84,7 @@ public class ValueClause {
         if (clazz == String.class) {
             return String.format(STRING_FORMAT.getFormat(), value);
         }
-        if(value == null) {
+        if (value == null) {
             return null;
         }
 
@@ -58,7 +92,7 @@ public class ValueClause {
     }
 
     public String toSql() {
-        return String.join(COMMA.getValue(), values);
+        return String.join(LINE_COMMA.getValue(), values);
     }
 
 
