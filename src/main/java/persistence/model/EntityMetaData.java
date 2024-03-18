0626ec428b5ebc5783244659267ab0d10dd48b5e
junseo <djawnstj44@naver.com>
2024-03-19 00:22:12 +0900

    feat: EntityMetaData 클래스 join 필드 추가

--- src/main/java/persistence/model/EntityMetaData.java
+++ src/main/java/persistence/model/EntityMetaData.java
@@ -1,8 +1,9 @@
 package persistence.model;
 
-import jakarta.persistence.Transient;
+import jakarta.persistence.*;
 import persistence.sql.QueryException;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
 import java.util.*;
 
@@ -11,6 +12,9 @@ public class EntityMetaData {
     private final String entityName;
     private final Class<?> entityClass;
     private final Map<String, Field> fields = new LinkedHashMap<>();
+    private final List<EntityJoinEntityField> joinFields = new ArrayList<>();
+
+    private static final Class<? extends Annotation>[] joinAnnotations = new Class[]{OneToOne.class, OneToMany.class, ManyToOne.class, ManyToMany.class};
 
     public EntityMetaData(final Class<?> entityClass) {
         this.entityName = entityClass.getName();
@@ -20,8 +24,23 @@ public class EntityMetaData {
 
     private void extractFields(final Class<?> entityClass) {
         Arrays.stream(entityClass.getDeclaredFields())
-                .filter(field -> !field.isAnnotationPresent(Transient.class))
-                .forEach(field -> this.fields.put(field.getName(), field));
+                .filter(this::isColumnField)
+                .forEach(field -> {
+                    if (isJoinColumnField(field)) {
+                        joinFields.add(new EntityJoinEntityField(field));
+                        return;
+                    }
+                    this.fields.put(field.getName(), field);
+                });
+    }
+
+    private boolean isColumnField(final Field field) {
+        return !field.isAnnotationPresent(Transient.class);
+    }
+
+    private boolean isJoinColumnField(final Field field) {
+        return Arrays.stream(joinAnnotations)
+                .anyMatch(field::isAnnotationPresent);
     }
 
     public String getEntityName() {
@@ -34,6 +53,10 @@ public class EntityMetaData {
 
     public List<Field> getFields() {
         return new ArrayList<>(this.fields.values());
+    }
+
+    public List<EntityJoinEntityField> getJoinFields() {
+        return Collections.unmodifiableList(this.joinFields);
     }
 
     public Map<String, Object> extractValues(final Object entity) {
