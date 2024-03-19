3bfdec1b62c5b8928fa7aedbddacb36725369698
Shin seongsu <tlstjdtn321@naver.com>
2024-03-20 01:07:49 +0900

    feat: 생성자를 추가

--- src/main/java/domain/LazyOrder.java
+++ src/main/java/domain/LazyOrder.java
@@ -3,6 +3,7 @@ package domain;
 import jakarta.persistence.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table(name = "lazy_orders")
@@ -17,4 +18,40 @@ public class LazyOrder {
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private List<OrderItem> orderItems;
+
+    public LazyOrder() {
+    }
+
+    public LazyOrder(final Long id,
+                     final String orderNumber,
+                     final List<OrderItem> orderItems) {
+        this.id = id;
+        this.orderNumber = orderNumber;
+        this.orderItems = orderItems;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getOrderNumber() {
+        return orderNumber;
+    }
+
+    public List<OrderItem> getOrderItems() {
+        return orderItems;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LazyOrder lazyOrder = (LazyOrder) o;
+        return Objects.equals(id, lazyOrder.id) && Objects.equals(orderNumber, lazyOrder.orderNumber) && Objects.equals(orderItems, lazyOrder.orderItems);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, orderNumber, orderItems);
+    }
 }
