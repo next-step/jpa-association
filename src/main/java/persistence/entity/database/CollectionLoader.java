2ae2c4db97f88837b1b1d226fe50ce8b5ac49b31
Dongyoon Lee <nine.cow@gmail.com>
2024-03-19 23:47:22 +0900

    style: 코드 정리

--- src/main/java/persistence/entity/database/CollectionLoader.java
+++ src/main/java/persistence/entity/database/CollectionLoader.java
@@ -2,7 +2,6 @@ package persistence.entity.database;
 
 import database.dialect.Dialect;
 import database.mapping.Association;
-import database.mapping.EntityMetadata;
 import database.mapping.EntityMetadataFactory;
 import database.mapping.rowmapper.JoinedRow;
 import database.mapping.rowmapper.JoinedRowMapper;
@@ -30,12 +29,15 @@ public class CollectionLoader {
         JoinedRowMapper<T> rowMapper = new JoinedRowMapper<>(clazz, dialect);
         List<JoinedRow<T>> joinedRows = jdbcTemplate.query(query, rowMapper);
 
-        return new JoinedRowsCombiner<>(joinedRows, clazz, getAssociations(clazz), entityLoader).merge();
+        // TODO: 조만간 clazz 대신 가져올 객체 통해서 얻을 수 있을거라고 믿고 여기 둠
+        List<Association> associations = getAssociations(clazz);
+
+        return new JoinedRowsCombiner<>(joinedRows, clazz, associations, entityLoader).merge();
+    }
+
+    private static <T> List<Association> getAssociations(Class<T> clazz) {
+        return EntityMetadataFactory.get(clazz).getAssociations();
     }
 
     // XXX: 뜬금없이 가져온 EntityMetadata
-    private static <T> List<Association> getAssociations(Class<T> clazz) {
-        EntityMetadata entityMetadata = EntityMetadataFactory.get(clazz);
-        return entityMetadata.getAssociations();
-    }
 }
