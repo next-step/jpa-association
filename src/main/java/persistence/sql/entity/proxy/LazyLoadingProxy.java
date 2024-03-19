c32ba672748eaed0b0ffb1dccd162143d88ff5c5
신성수 <tlstjdtn321@naver.com>
2024-03-19 21:15:43 +0900

    feat: LazyLoading 사용할수 있도록 추가

---
+++ src/main/java/persistence/sql/entity/proxy/LazyLoadingProxy.java
@@ -0,0 +1,39 @@
+package persistence.sql.entity.proxy;
+
+import net.sf.cglib.proxy.LazyLoader;
+import persistence.sql.entity.collection.CollectionLoader;
+import persistence.sql.entity.collection.CollectionPersister;
+
+import java.util.List;
+
+public class LazyLoadingProxy implements LazyLoader {
+
+    private final CollectionPersister collectionPersister;
+    private final CollectionLoader collectionLoader;
+    private final Class<?> clazz;
+    private final Object id;
+
+    public LazyLoadingProxy(final CollectionPersister collectionPersister,
+                            final CollectionLoader collectionLoader,
+                            final Class<?> clazz,
+                            final Object id) {
+        this.collectionPersister = collectionPersister;
+        this.collectionLoader = collectionLoader;
+        this.clazz = clazz;
+        this.id = id;
+    }
+
+    @Override
+    public Object loadObject() throws Exception {
+        List<Object> entity = collectionPersister.getEntity(clazz, id);
+        if(entity != null) {
+            return entity;
+        }
+
+        List<Object> selectEntity = collectionLoader.findById(clazz, id);
+        if(selectEntity == null) {
+            collectionPersister.addEntity(clazz, id, selectEntity);
+        }
+        return selectEntity;
+    }
+}
