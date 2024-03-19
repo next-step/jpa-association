3cd136ec7b94735cc586c282bf01e656dae199c0
JUNSEO <djawnstj44@naver.com>
2024-03-19 16:36:50 +0900

    feat: JoinColumn 역할 추가

--- src/main/java/persistence/sql/mapping/JoinColumn.java
+++ src/main/java/persistence/sql/mapping/JoinColumn.java
@@ -4,13 +4,25 @@ import persistence.sql.dml.ComparisonOperator;
 
 public class JoinColumn {
 
-    private final Column leftHandExpression;
-    private final Column rightHandExpression;
-    private final ComparisonOperator operator;
+    private final String leftHandExpression;
+    private final String rightHandExpression;
+    private final ComparisonOperator.Comparisons operator;
 
-    public JoinColumn(Column leftHandExpression, Column rightHandExpression, ComparisonOperator operator) {
+    public JoinColumn(final String leftHandExpression, final String rightHandExpression, final ComparisonOperator.Comparisons operator) {
         this.leftHandExpression = leftHandExpression;
         this.rightHandExpression = rightHandExpression;
         this.operator = operator;
+    }
+
+    public String getLeftHandExpression() {
+        return this.leftHandExpression;
+    }
+
+    public String getRightHandExpression() {
+        return this.rightHandExpression;
+    }
+
+    public String getOperator() {
+        return this.operator.getOperator();
     }
 }
