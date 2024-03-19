d291b17b4d444df3aab9ddcb0edc6efd85449a5a
JUNSEO <djawnstj44@naver.com>
2024-03-19 16:33:17 +0900

    feat: EntityMetaData 매핑 로직 수정

--- src/main/java/persistence/model/EntityMetaData.java
+++ src/main/java/persistence/model/EntityMetaData.java
@@ -1,46 +1,22 @@
 package persistence.model;
 
-import jakarta.persistence.*;
 import persistence.sql.QueryException;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class EntityMetaData {
 
     private final String entityName;
     private final Class<?> entityClass;
-    private final Map<String, Field> fields = new LinkedHashMap<>();
-    private final List<EntityJoinEntityField> joinFields = new ArrayList<>();
+    private final EntityFields entityFields;
 
-    private static final Class<? extends Annotation>[] joinAnnotations = new Class[]{OneToOne.class, OneToMany.class, ManyToOne.class, ManyToMany.class};
-
-    public EntityMetaData(final Class<?> entityClass) {
+    public EntityMetaData(final Class<?> entityClass, final EntityFields entityFields) {
         this.entityName = entityClass.getName();
         this.entityClass = entityClass;
-        extractFields(entityClass);
-    }
-
-    private void extractFields(final Class<?> entityClass) {
-        Arrays.stream(entityClass.getDeclaredFields())
-                .filter(this::isColumnField)
-                .forEach(field -> {
-                    if (isJoinColumnField(field)) {
-                        joinFields.add(new EntityJoinEntityField(field));
-                        return;
-                    }
-                    this.fields.put(field.getName(), field);
-                });
-    }
-
-    private boolean isColumnField(final Field field) {
-        return !field.isAnnotationPresent(Transient.class);
-    }
-
-    private boolean isJoinColumnField(final Field field) {
-        return Arrays.stream(joinAnnotations)
-                .anyMatch(field::isAnnotationPresent);
+        this.entityFields = entityFields;
     }
 
     public String getEntityName() {
@@ -52,11 +28,11 @@ public class EntityMetaData {
     }
 
     public List<Field> getFields() {
-        return new ArrayList<>(this.fields.values());
+        return this.entityFields.getFields().stream().map(EntityField::getField).toList();
     }
 
     public List<EntityJoinEntityField> getJoinFields() {
-        return Collections.unmodifiableList(this.joinFields);
+        return this.entityFields.getJoinFields();
     }
 
     public Map<String, Object> extractValues(final Object entity) {
@@ -66,10 +42,10 @@ public class EntityMetaData {
 
         final Map<String, Object> values = new HashMap<>();
 
-        fields.forEach((fieldName, field) -> {
+        getFields().forEach(field -> {
             try {
                 field.setAccessible(true);
-                values.put(fieldName, field.get(entity));
+                values.put(field.getName(), field.get(entity));
             } catch (IllegalAccessException e) {
                 throw new QueryException("can't get field " + field.getName() + " at " + entity.getClass().getName());
             } finally {
--- src/main/java/persistence/model/EntityMetaDataMapping.java
+++ src/main/java/persistence/model/EntityMetaDataMapping.java
@@ -1,5 +1,9 @@
 package persistence.model;
 
+import jakarta.persistence.Transient;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
@@ -7,10 +11,40 @@ import java.util.Objects;
 public class EntityMetaDataMapping {
 
     private static final Map<String, EntityMetaData> entityMetaDataMap = new HashMap<>();
+    private static final EntityJoinFieldMapping[] joinFieldMappings = initJoinFieldMappings();
+
+    private static EntityJoinFieldMapping[] initJoinFieldMappings() {
+        return new EntityJoinFieldMapping[]{
+                new EntityManyToManyFieldMapping(),
+                new EntityManyToOneFieldMapping(),
+                new EntityOneToManyFieldMapping(),
+                new EntityOneToOneFieldMapping()
+        };
+    }
 
     public static void putMetaData(final Class<?> entityClass) {
-        final EntityMetaData metaData = new EntityMetaData(entityClass);
+        final EntityFields entityFields = extractEntityFields(entityClass);
+        final EntityMetaData metaData = new EntityMetaData(entityClass, entityFields);
         entityMetaDataMap.put(metaData.getEntityName(), metaData);
+    }
+
+    private static EntityFields extractEntityFields(final Class<?> entityClass) {
+        final EntityFields entityFields = new EntityFields();
+        Arrays.stream(entityClass.getDeclaredFields())
+                .filter(EntityMetaDataMapping::isColumnField)
+                .forEach(field -> Arrays.stream(joinFieldMappings)
+                        .filter(mapping -> mapping.support(field))
+                        .findFirst()
+                        .ifPresentOrElse(
+                                mapping -> entityFields.addJoinField(mapping.create(field)),
+                                () -> entityFields.addField(new EntityField(field.getName(), field.getClass(), field))
+                        ));
+
+        return entityFields;
+    }
+
+    private static boolean isColumnField(final Field field) {
+        return !field.isAnnotationPresent(Transient.class);
     }
 
     public static EntityMetaData getMetaData(final String entityName) {
