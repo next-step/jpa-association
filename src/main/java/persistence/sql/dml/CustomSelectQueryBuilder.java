0e8b45e825db404106de176d1af4bc0625e6f728
parkje0927 <parkje09270@gmail.com>
2024-03-21 23:05:45 +0900

    refactor: step1 - EntityMetaData 초기화 시 EntityJoinMetaData 도 초기화 및 생성을 진행하고, 이에 따라 CustomSelectQueryBuilder 변수명 수정

--- src/main/java/persistence/sql/dml/CustomSelectQueryBuilder.java
+++ src/main/java/persistence/sql/dml/CustomSelectQueryBuilder.java
@@ -3,10 +3,10 @@ package persistence.sql.dml;
 import pojo.EntityJoinMetaData;
 import pojo.EntityMetaData;
 import pojo.FieldInfos;
+import pojo.FieldName;
 import pojo.IdField;
 
 import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
 
 import static constants.CommonConstants.COMMA;
 
@@ -14,55 +14,37 @@ public class CustomSelectQueryBuilder {
 
     private static final String FIND_BY_ID_JOIN_QUERY = "SELECT %s FROM %s LEFT JOIN %s ON %s = %s WHERE %s = %s;";
 
-    private final EntityJoinMetaData entityJoinMetaData; //Order
-    private final EntityMetaData owner; //OrderItem
+    private final EntityMetaData entityMetaData;
+    private final EntityJoinMetaData entityJoinMetaData;
 
-    public CustomSelectQueryBuilder(EntityJoinMetaData entityJoinMetaData) {
-        this.entityJoinMetaData = entityJoinMetaData;
-        this.owner = entityJoinMetaData.getOwner();
+    public CustomSelectQueryBuilder(EntityMetaData entityMetaData) {
+        this.entityMetaData = entityMetaData;
+        this.entityJoinMetaData = entityMetaData.getEntityJoinMetaData();
     }
 
-    public String findByIdJoinQuery1(Object entity, Class<?> clazz, Object condition) {
+    public String findByIdJoinQuery(Object entity, Class<?> clazz, Object condition) {
         Field field = new FieldInfos(clazz.getDeclaredFields()).getIdField();
         IdField idField = new IdField(field, entity);
 
         return String.format(FIND_BY_ID_JOIN_QUERY, getSelectData(),
-                entityJoinMetaData.getEntityName(), owner.getEntityName(),
-                entityJoinMetaData.getEntityName() + "." + idField.getFieldNameData(),
-                owner.getEntityName() + "." + entityJoinMetaData.joinColumnName(),
-                entityJoinMetaData.getEntityName() + "." + idField.getFieldNameData(), condition);
-    }
-
-    /**
-     * 아래 방법을 고민 중이라서 한번 피드백 부탁드립니다.
-     */
-    public String findByIdJoinQuery2(Object entity, Class<?> clazz, Object condition) {
-        Field field = new FieldInfos(clazz.getDeclaredFields()).getIdField();
-        IdField idField = new IdField(field, entity);
-
-        Field joinColumnField = new FieldInfos(clazz.getDeclaredFields()).getJoinColumnField();
-        Class<?> subClass = (Class<?>) ((ParameterizedType) joinColumnField.getGenericType()).getActualTypeArguments()[0];
-        EntityMetaData owner = new EntityMetaData(subClass, null);
-
-        return String.format(FIND_BY_ID_JOIN_QUERY, getSelectData(),
-                entityJoinMetaData.getEntityName(), owner.getEntityName(),
-                entityJoinMetaData.getEntityName() + "." + idField.getFieldNameData(),
-                owner.getEntityName() + "." + entityJoinMetaData.joinColumnName(),
-                entityJoinMetaData.getEntityName() + "." + idField.getFieldNameData(), condition);
+                entityMetaData.getEntityName(), entityJoinMetaData.getEntityName(),
+                entityMetaData.getEntityName() + "." + idField.getFieldNameData(),
+                entityJoinMetaData.getEntityName() + "." + entityJoinMetaData.getJoinColumnName(),
+                entityMetaData.getEntityName() + "." + idField.getFieldNameData(), condition);
     }
 
     private String getSelectData() {
-        String entityData = entityJoinMetaData.getEntityColumns()
+        String entityData = entityMetaData.getEntityColumns()
                 .stream()
                 .map(entityColumn -> entityColumn.getFieldName().getName())
-                .map(name -> entityJoinMetaData.getEntityName() + "." + name)
+                .map(name -> entityMetaData.getEntityName() + "." + name)
                 .reduce((o1, o2) -> String.join(COMMA, o1, o2))
                 .orElseThrow(() -> new IllegalStateException("Id 혹은 Column 타입이 없습니다."));
 
-        String ownerEntityData = owner.getEntityColumns()
+        String ownerEntityData = entityJoinMetaData.getFieldNames()
                 .stream()
-                .map(entityColumn -> entityColumn.getFieldName().getName())
-                .map(name -> owner.getEntityName() + "." + name)
+                .map(FieldName::getName)
+                .map(name -> entityJoinMetaData.getEntityName() + "." + name)
                 .reduce((o1, o2) -> String.join(COMMA, o1, o2))
                 .orElseThrow(() -> new IllegalStateException("Id 혹은 Column 타입이 없습니다."));
 
