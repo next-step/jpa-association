0d2a93506f6d0b89cade6b3692bb7fe1b985fe24
JUNSEO <djawnstj44@naver.com>
2024-03-21 10:15:38 +0900

    feat: ReflectionUtils 클래스 유틸 메서드 추가

--- src/main/java/persistence/ReflectionUtils.java
+++ src/main/java/persistence/ReflectionUtils.java
@@ -5,6 +5,9 @@ import persistence.sql.QueryException;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class ReflectionUtils {
 
@@ -24,13 +27,71 @@ public class ReflectionUtils {
         }
     }
 
-    public static <T> void setFieldValue(final Field field, final T entity, final Object value) {
+    public static <T> void setFieldValue(final Field field, final T object, final Object value) {
         try {
+            if (isListType(field)) {
+                setListFieldValue(field, object, value);
+                return;
+            }
+
+            if (isDefinedField(field, object)) {
+                return;
+            }
+
             field.setAccessible(true);
-            field.set(entity, value);
+            field.set(object, value);
         } catch (IllegalAccessException e) {
-            throw new QueryException("can't set field " + field.getName() + " at " + entity.getClass().getName());
+            throw new QueryException("can't set field " + field.getName() + " at " + object.getClass().getName());
         }
     }
 
+    private static <T> void setListFieldValue(final Field field, final T object, final Object value) {
+        try {
+            field.setAccessible(true);
+            Object list = field.get(object);
+
+            if (Objects.isNull(list)) {
+                list = new ArrayList<>();
+                field.set(object, list);
+            }
+
+            ((List) list).add(value);
+        } catch (IllegalAccessException e) {
+            throw new QueryException("can't set field " + field.getName() + " at " + object.getClass().getName());
+        } finally {
+            field.setAccessible(false);
+        }
+    }
+
+    private static boolean isListType(final Field field) {
+        return List.class.isAssignableFrom(field.getType());
+    }
+
+    private static boolean isDefinedField(final Field field, final Object object) {
+        try {
+            field.setAccessible(true);
+            final Object value = field.get(object);
+
+            return Objects.nonNull(value);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } finally {
+            field.setAccessible(false);
+        }
+    }
+
+    private static Object getFieldValue(final Field field, final Object object) {
+        try {
+            field.setAccessible(true);
+            return field.get(object);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } finally {
+            field.setAccessible(false);
+        }
+    }
+
+    public static boolean isDifferentFieldValue(final Field field, final Object object, final Object value) {
+        return !Objects.deepEquals(getFieldValue(field, object), value);
+    }
 }
