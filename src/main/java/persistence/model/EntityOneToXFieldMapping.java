dce5e617f4dd3d513d1bb6ad26e6b864143190a1
JUNSEO <djawnstj44@naver.com>
2024-03-21 10:16:50 +0900

    refactor: EntityOneToXFieldMapping 상위 클래스의 메서드 접근제어자 변경에 따른 getEntityType 메서드 접근 제어자 수정

--- src/main/java/persistence/model/EntityOneToXFieldMapping.java
+++ src/main/java/persistence/model/EntityOneToXFieldMapping.java
@@ -6,7 +6,7 @@ import java.lang.reflect.ParameterizedType;
 public abstract class EntityOneToXFieldMapping extends EntityJoinFieldMapping {
 
     @Override
-    protected Class<?> getEntityType(final Field field) {
+    public Class<?> getEntityType(final Field field) {
         return (Class<?>) (((ParameterizedType) field.getGenericType()).getActualTypeArguments()[0]);
     }
 
