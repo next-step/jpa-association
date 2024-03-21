5e7d59a949c3099c125ed4c99f10e79223a427f1
JUNSEO <djawnstj44@naver.com>
2024-03-21 10:13:15 +0900

    feat: EntityFields 클래스 @Id 필드에 관련한 역할 추가

--- src/main/java/persistence/model/EntityFields.java
+++ src/main/java/persistence/model/EntityFields.java
@@ -1,16 +1,25 @@
 package persistence.model;
 
-import java.lang.reflect.Field;
+import jakarta.persistence.Id;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class EntityFields {
+    private EntityField idField;
     private final List<EntityField> fields = new ArrayList<>();
     private final List<EntityJoinEntityField> joinFields = new ArrayList<>();
 
     public void addField(final EntityField field) {
+        if (field.getField().isAnnotationPresent(Id.class)) {
+            this.idField = field;
+        }
         this.fields.add(field);
+    }
+
+    public EntityField getIdField() {
+        return this.idField;
     }
 
     public void addJoinField(final EntityJoinEntityField joinEntityField) {
