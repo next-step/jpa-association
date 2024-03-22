52adf82721223c4f77a56b3cc57cb36c19d241e3
JUNSEO <djawnstj44@naver.com>
2024-03-22 16:31:48 +0900

    refactor: AbstractPersistentClass 삭제
    PersistentClass 와 PersistentJoinClass 의 상속을 위한 AbstractPersistentClass 를 삭제하였고,
    Map 으로 관리하던 필드들의 값은 PersistentClassFields 클래스를 만들어 일급 컬렉션으로 관리되도록 하였다.

--- src/main/java/persistence/entity/PersistentClass.java
+++ src/main/java/persistence/entity/PersistentClass.java
@@ -1,28 +1,37 @@
 package persistence.entity;
 
 import persistence.ReflectionUtils;
+import persistence.model.EntityFields;
 import persistence.model.EntityJoinEntityField;
+import persistence.model.EntityMetaData;
 import persistence.model.EntityMetaDataMapping;
+import persistence.sql.mapping.ColumnBinder;
 
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class PersistentClass<T> extends AbstractPersistentClass {
+public class PersistentClass<T> {
 
+    private final Class<T> clazz;
+    private T entity;
+    private final EntityMetaData entityMetaData;
     private final List<PersistentJoinClass> persistentJoinClasses = new ArrayList<>();
+    private final PersistentClassFields fields = new PersistentClassFields();
 
     public PersistentClass(final Class<T> clazz) {
-        super(clazz, EntityMetaDataMapping.getMetaData(clazz.getName()));
+        this.clazz = clazz;
         this.entity = ReflectionUtils.createInstance(this.clazz);
+        this.entityMetaData = EntityMetaDataMapping.getMetaData(clazz.getName());
         this.entityMetaData.getJoinFields().stream()
                 .filter(EntityJoinEntityField::isNotLazy)
                 .forEach(this::addJoinedEntityClass);
     }
 
-    @Override
     public void initPersistentClass() {
-        super.initPersistentClass();
-        this.persistentJoinClasses.forEach(PersistentJoinClass::initPersistentClass);
+        this.fields.init();
+        this.persistentJoinClasses.forEach(PersistentJoinClass::clearValue);
     }
 
     private void addJoinedEntityClass(final EntityJoinEntityField joinEntityField) {
@@ -31,7 +40,7 @@ public class PersistentClass<T> extends AbstractPersistentClass {
 
     public void setFieldValue(final String tableName, final String columnLabel, final Object value) {
         if (entityMetaData.isSameTableName(tableName)) {
-            setInternalFieldValue(columnLabel, value);
+            fields.setFieldValue(columnLabel, value);
             return;
         }
 
@@ -41,15 +50,36 @@ public class PersistentClass<T> extends AbstractPersistentClass {
     private void setJoinClassFieldValue(final String tableName, final String columnLabel, final Object value) {
         this.persistentJoinClasses.stream().filter(clazz -> clazz.isSameTableName(tableName))
                 .findFirst()
-                .ifPresent(clazz -> clazz.setInternalFieldValue(columnLabel, value));
+                .ifPresent(clazz -> clazz.setFieldValue(columnLabel, value));
     }
 
-    @Override
     public T getEntity() {
-        super.getEntity();
+        final Field idField = this.entityMetaData.getIdField();
+        final String idColumnName = ColumnBinder.toColumnName(idField);
+
+        if (ReflectionUtils.isDifferentFieldValue(idField, this.entity, this.fields.getFieldValue(idColumnName))) {
+            this.entity = initEntity(this.clazz);
+        }
+
         this.persistentJoinClasses.forEach(clazz -> ReflectionUtils.setFieldValue(clazz.getJoinedEntityField(), this.entity, clazz.getEntity()));
 
-        return (T) this.entity;
+        return this.entity;
+    }
+
+    private T initEntity(final Class<T> clazz) {
+        final T entity = ReflectionUtils.createInstance(clazz);
+        this.entityMetaData.getFields().forEach(field -> {
+            final String columnName = ColumnBinder.toColumnName(field);
+            final Object columnValue = this.fields.getFieldValue(columnName);
+
+            if (Objects.isNull(columnValue)) {
+                return;
+            }
+
+            ReflectionUtils.setFieldValue(field, entity, columnValue);
+        });
+
+        return entity;
     }
 
 }
--- src/main/java/persistence/entity/PersistentJoinClass.java
+++ src/main/java/persistence/entity/PersistentJoinClass.java
@@ -2,17 +2,28 @@ package persistence.entity;
 
 import persistence.ReflectionUtils;
 import persistence.model.EntityJoinEntityField;
+import persistence.model.EntityMetaData;
+import persistence.model.EntityMetaDataMapping;
+import persistence.sql.mapping.ColumnBinder;
 
 import java.lang.reflect.Field;
+import java.util.Objects;
 
-public class PersistentJoinClass extends AbstractPersistentClass {
+public class PersistentJoinClass {
 
+    private final Class<?> clazz;
+    private final EntityMetaData entityMetaData;
     private final EntityJoinEntityField joinEntityField;
+    private final PersistentClassFields fields = new PersistentClassFields();
 
     public PersistentJoinClass(EntityJoinEntityField joinEntityField) {
-        super(joinEntityField.getFieldType(), joinEntityField.getMetaData());
+        this.clazz = joinEntityField.getFieldType();
         this.joinEntityField = joinEntityField;
-        this.entity = ReflectionUtils.createInstance(this.clazz);
+        this.entityMetaData = EntityMetaDataMapping.getMetaData(this.clazz.getName());
+    }
+
+    public void clearValue() {
+        this.fields.init();
     }
 
     public boolean isSameTableName(final String tableName) {
@@ -21,5 +32,30 @@ public class PersistentJoinClass extends AbstractPersistentClass {
 
     public Field getJoinedEntityField() {
         return this.joinEntityField.getField();
+    }
+
+    public void setFieldValue(final String columnLabel, final Object value) {
+        fields.setFieldValue(columnLabel, value);
+    }
+
+    public Object getEntity() {
+        if (this.fields.isEmpty()) {
+            return null;
+        }
+
+        final Object entity = ReflectionUtils.createInstance(clazz);
+
+        this.entityMetaData.getFields().forEach(field -> {
+            final String columnName = ColumnBinder.toColumnName(field);
+            final Object columnValue = fields.getFieldValue(columnName);
+
+            if (Objects.isNull(columnValue)) {
+                return;
+            }
+
+            ReflectionUtils.setFieldValue(field, entity, columnValue);
+        });
+
+        return entity;
     }
 }
