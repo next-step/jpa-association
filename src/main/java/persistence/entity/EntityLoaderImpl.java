f29d118bbef26f8e76d7c2822eb33a2024385851
parkje0927 <parkje09270@gmail.com>
2024-03-25 22:56:18 +0900

    feat: step1 - 요구사항 2번을 위해 loader 메소드 추가

--- src/main/java/persistence/entity/EntityLoaderImpl.java
+++ src/main/java/persistence/entity/EntityLoaderImpl.java
@@ -2,7 +2,9 @@ package persistence.entity;
 
 import jdbc.JdbcTemplate;
 import jdbc.RowMapperImpl;
+import persistence.sql.dml.CustomSelectQueryBuilder;
 import persistence.sql.dml.SelectQueryBuilder;
+import pojo.EntityJoinMetaData;
 import pojo.EntityMetaData;
 
 import java.util.List;
@@ -26,5 +28,25 @@ public class EntityLoaderImpl implements EntityLoader {
     public <T> List<T> findAll(Class<T> clazz) {
         SelectQueryBuilder selectQueryBuilder = new SelectQueryBuilder(entityMetaData);
         return jdbcTemplate.query(selectQueryBuilder.findAllQuery(), new RowMapperImpl<>(clazz));
+    }
+
+    //연관관계가 있는 경우 & eager 타입만 고려
+    @Override
+    public <T> List<T> findByIdWithAssociation(Class<T> clazz, Object entity, Object condition) {
+        CustomSelectQueryBuilder customSelectQueryBuilder = new CustomSelectQueryBuilder(entityMetaData);
+
+        EntityJoinMetaData entityJoinMetaData = entityMetaData.getEntityJoinMetaData();
+        if (!entityJoinMetaData.isLazy()) {
+            return eagerTypeQuery(customSelectQueryBuilder, clazz, entity);
+        }
+
+        //TODO 추후 lazy 타입 넣을 예정
+        return jdbcTemplate.query(customSelectQueryBuilder.findByIdJoinQuery(entity, clazz), new RowMapperImpl<>(clazz));
+
+    }
+
+    private <T> List<T> eagerTypeQuery(CustomSelectQueryBuilder customSelectQueryBuilder, Class<T> clazz, Object entity) {
+        return jdbcTemplate.query(customSelectQueryBuilder.findByIdJoinQuery(entity, clazz), new RowMapperImpl<>(clazz));
+
     }
 }
