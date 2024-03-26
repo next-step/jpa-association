7b67e65d72ad8bf47b7e8c493dd7eedac5c776c9
JUNSEO <djawnstj44@naver.com>
2024-03-26 17:29:17 +0900

    refactor: SingleEntityLoader load 메서드 로직 수정
    조인 상태에 따라 적절한 RowMapper 를 사용하도록 수정하였다.

--- src/main/java/persistence/entity/loader/SingleEntityLoader.java
+++ src/main/java/persistence/entity/loader/SingleEntityLoader.java
@@ -1,7 +1,11 @@
 package persistence.entity.loader;
 
 import jdbc.JdbcTemplate;
+import jdbc.RowMapper;
+import persistence.ReflectionUtils;
+import persistence.entity.CollectionEntityRowMapper;
 import persistence.entity.EntityRowMapper;
+import persistence.model.*;
 import persistence.sql.QueryException;
 import persistence.sql.dml.*;
 import persistence.sql.mapping.Column;
@@ -11,29 +15,55 @@ import persistence.sql.mapping.TableBinder;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class SingleEntityLoader implements EntityLoader {
 
     private final TableBinder tableBinder;
+    private final CollectionPersistentClassBinder collectionPersistentClassBinder;
     private final DmlQueryBuilder dmlQueryBuilder;
     private final JdbcTemplate jdbcTemplate;
 
-    public SingleEntityLoader(TableBinder tableBinder, DmlQueryBuilder dmlQueryBuilder, JdbcTemplate jdbcTemplate) {
+    public SingleEntityLoader(final TableBinder tableBinder, final CollectionPersistentClassBinder collectionPersistentClassBinder, final DmlQueryBuilder dmlQueryBuilder, final JdbcTemplate jdbcTemplate) {
         this.tableBinder = tableBinder;
+        this.collectionPersistentClassBinder = collectionPersistentClassBinder;
         this.dmlQueryBuilder = dmlQueryBuilder;
         this.jdbcTemplate = jdbcTemplate;
     }
 
     @Override
     public <T> List<T> load(final Class<T> clazz, final Object key) {
+        final PersistentClass<T> persistentClass = PersistentClassMapping.getPersistentClass(clazz);
         final Select select = generateSelect(clazz, key);
 
         final String selectQuery = dmlQueryBuilder.buildSelectQuery(select);
         log.debug("\n" + selectQuery);
-        final EntityRowMapper<T> entityRowMapper = new EntityRowMapper<>(clazz);
 
-        return jdbcTemplate.query(selectQuery, entityRowMapper)
+        final Optional<EntityJoinField> eagerJoinField = persistentClass.getJoinFields()
+                .stream().filter(EntityJoinField::isEager)
+                .findFirst();
+
+        if (eagerJoinField.isPresent()) {
+            final EntityJoinField entityJoinField = eagerJoinField.get();
+            final CollectionPersistentClass collectionPersistentClass = this.collectionPersistentClassBinder.getCollectionPersistentClass(ReflectionUtils.mapToGenericClassName(entityJoinField.getField()));
+
+            return queryWithEagerColumn(clazz, selectQuery, collectionPersistentClass);
+        }
+
+        final RowMapper<T> rowMApper = new EntityRowMapper<>(persistentClass);
+
+        return jdbcTemplate.query(selectQuery, rowMApper)
+                .stream()
+                .filter(Objects::nonNull)
+                .distinct()
+                .collect(Collectors.toUnmodifiableList());
+    }
+
+    private <T> List<T> queryWithEagerColumn(final Class<T> clazz, final String selectQuery, final CollectionPersistentClass collectionPersistentClass) {
+        final RowMapper<T> rowMApper = new CollectionEntityRowMapper<>(clazz, collectionPersistentClass);
+
+        return jdbcTemplate.query(selectQuery, rowMApper)
                 .stream()
                 .filter(Objects::nonNull)
                 .distinct()
@@ -41,7 +71,7 @@ public class SingleEntityLoader implements EntityLoader {
     }
 
     private <T> Select generateSelect(final Class<T> clazz, final Object key) {
-        final Table table = tableBinder.createTable(clazz);
+        final Table table = tableBinder.createTable(clazz, this.collectionPersistentClassBinder);
 
         final Select select = new Select(table);
 
