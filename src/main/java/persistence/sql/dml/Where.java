a3bb0a61012161d37572d49d412ab684b9f711ed
JUNSEO <djawnstj44@naver.com>
2024-03-29 16:50:02 +0900

    refactor: Where 클래스 테이블 이름을 Column 클래스에서 가져오는 로직에서 tableName 을 상태값으로 가지도록 수정

--- src/main/java/persistence/sql/dml/Where.java
+++ src/main/java/persistence/sql/dml/Where.java
@@ -5,15 +5,18 @@ import persistence.sql.mapping.Value;
 
 public class Where {
 
+    private final String tableName;
     private final Column column;
-
     private final Value value;
-
     private LogicalOperator logicalOperator;
-
     private final WhereOperator whereOperator;
 
     public Where(final Column column, final Value value, final LogicalOperator logicalOperator, final WhereOperator whereOperator) {
+        this(column.getTableName(), column, value, logicalOperator, whereOperator);
+    }
+
+    public Where(final String tableName, final Column column, final Value value, final LogicalOperator logicalOperator, final WhereOperator whereOperator) {
+        this.tableName = tableName;
         this.column = column;
         this.value = value;
         this.logicalOperator = logicalOperator;
@@ -21,7 +24,7 @@ public class Where {
     }
 
     public String getColumnTableName() {
-        return this.column.getTableName();
+        return this.tableName;
     }
 
     public String getColumnName() {
