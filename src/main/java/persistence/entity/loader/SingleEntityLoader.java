cdd3f6497e5f5d0c2a5f8d1159880472fb73f47b
JUNSEO <djawnstj44@naver.com>
2024-03-29 16:51:10 +0900

    feat: SingleEntityLoader 클래스 load 메서드에서 조인 필드 가져오는 코드 수정
    기존 join 코드를 가져올 때, EAGER 타입의 필드만 가져왔지만,
    지연 로딩 기능 추가를 위해 EAGER/LAZY 상태를 구분해 필드를 가져오도록 수정하였다.

--- src/main/java/persistence/entity/loader/SingleEntityLoader.java
+++ src/main/java/persistence/entity/loader/SingleEntityLoader.java
@@ -2,18 +2,14 @@ package persistence.entity.loader;
 
 import jdbc.JdbcTemplate;
 import jdbc.RowMapper;
+import persistence.ReflectionUtils;
 import persistence.entity.SingleEntityRowMapper;
 import persistence.model.*;
 import persistence.sql.QueryException;
 import persistence.sql.dml.*;
-import persistence.sql.mapping.Column;
-import persistence.sql.mapping.PrimaryKey;
-import persistence.sql.mapping.Table;
-import persistence.sql.mapping.TableBinder;
+import persistence.sql.mapping.*;
 
-import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class SingleEntityLoader implements EntityLoader {
@@ -38,21 +34,42 @@ public class SingleEntityLoader implements EntityLoader {
         final String selectQuery = dmlQueryBuilder.buildSelectQuery(select);
         log.debug("\n" + selectQuery);
 
-        final Optional<EntityJoinField> eagerJoinField = persistentClass.getFields()
+        final Map<Boolean, List<EntityJoinField>> joinFieldsMap = persistentClass.getFields()
                 .stream()
                 .filter(AbstractEntityField::isJoinField)
                 .map(joinField -> (EntityJoinField) joinField)
-                .filter(EntityJoinField::isEager)
-                .findFirst();
+                .collect(Collectors.partitioningBy(EntityJoinField::isEager));
 
+        final Optional<EntityJoinField> eagerJoinField = joinFieldsMap.getOrDefault(true, Collections.emptyList())
+                .stream().findFirst();
+
+        final List<EntityJoinField> lazyJoinFields = joinFieldsMap.getOrDefault(false, Collections.emptyList());
+
+        final List<T> entities = getEntities(persistentClass, eagerJoinField, selectQuery);
+
+        if (!lazyJoinFields.isEmpty()) {
+            final EntityId entityId = persistentClass.getEntityId();
+            final Object[] idValues = entities.stream().map(entity -> ReflectionUtils.getFieldValue(entityId.getField(), entity)).toArray();
+
+            final Select[] selects = lazyJoinFields.stream().map(field -> generateSelect(field.getEntityClass(), select.getTable().getPrimaryKey().getColumns().get(0), idValues[0])).toArray(Select[]::new);
+
+            Arrays.stream(selects)
+                    .map(dmlQueryBuilder::buildSelectQuery)
+                    .forEach(System.out::println);
+        }
+
+        return entities;
+    }
+
+    private <T> List<T> getEntities(final PersistentClass<T> persistentClass, final Optional<EntityJoinField> eagerJoinField, final String query) {
         if (eagerJoinField.isPresent()) {
             final CollectionEntityLoader collectionEntityLoader = new CollectionEntityLoader(jdbcTemplate);
-            return collectionEntityLoader.queryWithEagerColumn(clazz, eagerJoinField.get(), this.collectionPersistentClassBinder, selectQuery);
+            return collectionEntityLoader.queryWithEagerColumn(persistentClass.getEntityClass(), eagerJoinField.get(), this.collectionPersistentClassBinder, query);
         }
 
         final RowMapper<T> rowMApper = new SingleEntityRowMapper<>(persistentClass);
 
-        return jdbcTemplate.query(selectQuery, rowMApper)
+        return jdbcTemplate.query(query, rowMApper)
                 .stream()
                 .filter(Objects::nonNull)
                 .distinct()
@@ -83,5 +100,16 @@ public class SingleEntityLoader implements EntityLoader {
                 .stream()
                 .findFirst()
                 .orElseThrow(() -> new QueryException("not found id column"));
+    }
+
+    private <T> Select generateSelect(final Class<T> clazz, final Column column, final Object value) {
+        final Table table = tableBinder.createTable(clazz, this.collectionPersistentClassBinder);
+
+        final Select select = new Select(table);
+        final Value columnValue = new Value(value.getClass(), ColumnTypeMapper.getInstance().toSqlType(value.getClass()), value);
+
+        select.addWhere(new Where(table.getName(), column, columnValue, LogicalOperator.NONE, new ComparisonOperator(ComparisonOperator.Comparisons.EQ)));
+
+        return select;
     }
 }
