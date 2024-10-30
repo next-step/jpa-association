b2f904f242ca18773e533d28ee56881c1a568d3d
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-10-31 02:25:18 +0900

    feat: Order OrderItem 생성

---
+++ src/main/java/entity/Order.java
@@ -0,0 +1,24 @@
+package entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String orderNumber;
+
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "order_id")
+    private List<OrderItem> orderItems;
+
+    public Order(Long id) {
+        this.id = id;
+    }
+}
