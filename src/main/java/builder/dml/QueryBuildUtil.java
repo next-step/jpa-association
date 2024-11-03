96c8106fd74e4180f71c41df235394f6f073695e
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-11-04 03:10:13 +0900

    refactor: Join여부확인하여 별칭 붙혀주는 작업 QueryBuildUtil로 wrap

---
+++ src/main/java/builder/dml/QueryBuildUtil.java
@@ -0,0 +1,49 @@
+package builder.dml;
+
+import java.util.stream.Collectors;
+
+public class QueryBuildUtil {
+
+    private static final String UNDER_BAR = "_";
+    private static final String DOT = ".";
+    private static final String BLANK = " ";
+    private static final String COMMA = ", ";
+
+    public static String getAlias(String table) {
+        return table + UNDER_BAR;
+    }
+
+    public static String getContainAliasTableName(String tableName, String alias) {
+        return tableName + BLANK + alias;
+    }
+
+    public static String getContainAliasColumnName(String columnName, String alias) {
+        return alias + DOT + columnName;
+    }
+
+    public static String getTableName(EntityData entityData) {
+        if (entityData.checkJoin()) {
+            return entityData.getTableName() + BLANK + entityData.getAlias();
+        }
+        return entityData.getTableName();
+    }
+
+    public static String getColumnNames(EntityData entityData) {
+        if (entityData.checkJoin()) {
+            String baseColumnNames = entityData.getEntityColumn().getColumns().stream()
+                    .map(columnData -> getContainAliasColumnName(columnData.getColumnName(), entityData.getAlias())) // aliasColumn 메서드 사용
+                    .collect(Collectors.joining(COMMA));
+
+            JoinEntityData joinEntityData = entityData.getJoinEntity().getJoinEntityData().getFirst();
+
+            String joinColumn = joinEntityData.getJoinColumnData().getColumns().stream()
+                    .map(joinColumnData -> getContainAliasColumnName(joinColumnData.getColumnName(), joinEntityData.getAlias()))
+                    .collect(Collectors.joining(COMMA));
+
+            return baseColumnNames + COMMA + joinColumn;
+        }
+
+        return entityData.getEntityColumn().getColumnNames();
+    }
+
+}
