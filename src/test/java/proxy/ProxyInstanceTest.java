ea85ca8614ca2d45dd2cb76de47ef7ad4de9871b
Sangki Han <68369248+SangkiHan@users.noreply.github.com>
2024-11-06 02:03:43 +0900

    test: 테스트코드

---
+++ src/test/java/proxy/ProxyInstanceTest.java
@@ -0,0 +1,17 @@
+package proxy;
+
+import entity.Person;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.List;
+
+class ProxyInstanceTest {
+
+    @DisplayName("")
+    @Test
+    void upperTest() {
+    }
+}
